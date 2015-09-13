c9cece0ea660ecf4f1e874fa74604a74d4530f73
xiaozhang535 <hqzhang1983@gmail.com>
2015-09-13 18:04:25 +0800

    public_tools

--- initial.sh
+++ initial.sh
@@ -16,14 +16,25 @@ cd /usr/local/bin/;ln -s -T /usr/bin/cscope cscope;cd -;
 cd /root/
 mkdir ticket_src;cd ticket_src;
 git init;
-git remote add origin https://github.com/gogofly/sola.git;
-git config --global user.email "ginozhang@vip.qq.com"
+#git remote add origin https://github.com/gogofly/sola.git;
+git remote add origin https://github.com/xiaozhang535/sola.git;
+#git config --global user.email "ginozhang@vip.qq.com"
+git config user.name xiaozhang535
+git config user.email hqzhang1983@gmail.com
 git pull -u origin master;
 #403: vim .git/config; url = https://gogofly@github.com/gogofly/sola.git
 
 #common
-mkdir /usr/local/avs;cp public_tools.tgz /usr/local/avs/;
-cd /usr/local/avs/;tar -zxvf public_tools.tgz;cd -;
+#mkdir /usr/local/avs;cp public_tools.tgz /usr/local/avs/;
+#cd /usr/local/avs/;tar -zxvf public_tools.tgz;cd -;
+mkdir /usr/local/avs/public_tools/;cd /usr/local/avs/public_tools/;
+git init;
+git remote add origin https://github.com/xiaozhang535/public_tools.git;
+#git clone https://github.com/xiaozhang535/public_tools.git public_tools/
+git config user.name xiaozhang535
+git config user.email hqzhang1983@gmail.com
+git pull -u origin master;
+cd -;
 
 #lib
 yum -y install lrzsz
